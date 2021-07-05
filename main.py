911266137593460a28d44dc6f04b97ab1b4b664a
Leandro Macato <shadowslayerhero@gmail.com>
2021-07-05 23:22:09 +0800

    updated module working on second problem

--- main.py
+++ main.py
@@ -1,56 +1,34 @@
-# https://whitemech.github.io/pythomata/quickstart/
+# https://pypi.org/project/automata-lib/ AUTOMATLIB
+# https://pypi.org/project/visual-automata/ VISUAL AUTOMATA
 
-from pythomata import SimpleDFA
-# choco install graphviz
+# Problem
+# ( 1 + 0 )* (11 + 00 + 101 + 010) ( 1 + 0 + 11 + 00 + 101 )* ( 11 + 00 )
+# ( 11+ 00 + 101 )* ( 1 + 0 ) ( 1 + 0 + 11 )*
 
-states = {"q0", "q1", "q2", "q3"}
-alphabet = {"0", "1"}
-initial_state = "q0"
-accepting_states = {"q0"}
+from automata.fa.dfa import DFA
 
-
-transition_function = {
-    "q0": {
-        "0": "q2",
-        "1": "q1",
+dfa = DFA(
+    states={'q0', 'q1', 'q2', 'q3', 'q4',
+            'q5', 'q6', 'q7', 'q8'},
+    input_symbols={'0', '1'},
+    transitions={
+        'q0': {'0': 'q1', '1': 'q2'},
+        'q1': {'0': 'q4', '1': 'q3'},
+        'q2': {'0': 'q3', '1': 'q4'},
+        'q3': {'0': 'q4', '1': 'q4'},
+        'q4': {'0': 'q5', '1': 'q6'},
+        'q5': {'0': 'q7', '1': 'q6'},
+        'q6': {'0': 'q5', '1': 'q7'},
+        'q7': {'0': 'q8', '1': 'q8'},
+        'q8': {'0': 'q8', '1': 'q8'}
     },
-    "q1": {
-        "0": "q3",
-        "1": "q0",
-    },
-    "q2": {
-        "0": "q0",
-        "1": "q3",
-    },
-    "q3": {
-        "0": "q1",
-        "1": "q2",
-    }
-}
-
-
-automaton = SimpleDFA(
-    states=states,
-    alphabet=alphabet,
-    initial_state=initial_state,
-    accepting_states=accepting_states,
-    transition_function=transition_function,
+    initial_state='q0',
+    final_states={'q8'}
 )
 
-print(automaton.alphabet.size)  # 2
+my_input_str = input("Enter String:")
 
-list(automaton.alphabet)  # ['0', '1']
-
-automaton.accepts("")         # True
-automaton.accepts("0")        # False - only one '0'
-automaton.accepts("1")        # False - only one '1'
-automaton.accepts("00")       # True
-automaton.accepts("11")       # True
-automaton.accepts("01" * 42)  # True
-
-
-digraph = automaton.to_graphviz()
-
-
-# Renger to DFA
-digraph.render("sample_automaton")
+if dfa.accepts_input(my_input_str):
+    print('accepted')
+else:
+    print('rejected')
