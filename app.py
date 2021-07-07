0115e6654cb473982a4a64ba34681f05d261d70a
Leandro Macato <shadowslayerhero@gmail.com>
2021-07-07 11:01:14 +0800

    working both expression w/ errors

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 
 # TODO
 # WHILE NOTHING IS IN INPUT BOX DONT DISPLAY SHIT
+# When switching expression there will be an error becoz naka submit parin yung input
 
 import streamlit as st
 from automata.fa.dfa import DFA
@@ -27,7 +28,39 @@ input_box = st.sidebar.selectbox(
 
 # Page Control
 if input_box == q1:
+    first_dfa = DFA(
+        states={'q0', 'q1', 'q2', 'q3', 'q4',
+                'q5', 'q6', 'q7', 'q8'},
+        input_symbols={'a', 'b'},
+        transitions={
+            'q0': {'a': 'q1', 'b': 'q2'},
+            'q1': {'a': 'q2', 'b': 'q2'},
+            'q2': {'a': 'q3', 'b': 'q5'},
+            'q3': {'a': 'q4', 'b': 'q5'},
+            'q4': {'a': 'q7', 'b': 'q5'},
+            'q5': {'a': 'q3', 'b': 'q6'},
+            'q6': {'a': 'q3', 'b': 'q7'},
+            'q7': {'a': 'q8', 'b': 'q8'},
+            'q8': {'a': 'q8', 'b': 'q8'}
+        },
+        initial_state='q0',
+        final_states={'q8'}
+    )
+
+    vis_dfa = VisualDFA(first_dfa)  # Create Visual DFA from DFA
     st.write(q1)
+    with st.form("form"):
+        first_user_input = st.text_input(
+            label="Enter String To Check If Valid:")
+        submit_button = st.form_submit_button(label="Submit")
+
+    if first_dfa.accepts_input(first_user_input):
+        st.write("ACCEPTED!")
+        st.write(vis_dfa.show_diagram(first_user_input))
+    else:
+        st.write("REJECTED!")
+        st.write(vis_dfa.show_diagram(first_user_input))
+
 elif input_box == q2:
     dfa = DFA(
         states={'q0', 'q1', 'q2', 'q3', 'q4',
@@ -53,16 +86,17 @@ elif input_box == q2:
 
     # User input Box
     with st.form("form"):
-        user_input = st.text_input(label="Enter String To Check If Valid:")
+        second_user_input = st.text_input(
+            label="Enter String To Check If Valid:")
         submit_button = st.form_submit_button(label="Submit")
 
     # Check if string is valid
-    if dfa.accepts_input(user_input):
+    if dfa.accepts_input(second_user_input):
         st.write("ACCEPTED!")
-        st.write(vis_dfa.show_diagram(user_input))
+        st.write(vis_dfa.show_diagram(second_user_input))
     else:
         st.write("REJECTED!")
-        st.write(vis_dfa.show_diagram(user_input))
+        st.write(vis_dfa.show_diagram(second_user_input))
 
     # Visual DFA
 
